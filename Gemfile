2302be402509b21378bbd365dbd1005cdd64ee53
Kate <katepdonahue@gmail.com>
2014-01-06 21:02:56 -0500

    Initial Sinatra file structure

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "https://rubygems.org/"
+
+ruby '2.0.0'
+gem "sinatra"
+gem "haml"
+gem "sass"
+gem "compass"
+
+
+group :development do
+  gem "shotgun"
+  gem "tux"
+end
---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+Bundler.require
+require './lib/email'
+
+
+class App < Sinatra::Application
+
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'views'
+    end
+
+    set :haml, { :formt => :html5 }
+    set :scss, Compass.sass_engine_options
+  end
+
+  get '/stylesheet.css' do
+    scss :input
+  end
+
+
+
+  get '/' do
+    haml :index
+  end
+ 
+end
+
+
+
